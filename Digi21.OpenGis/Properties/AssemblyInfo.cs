2ffe26ee02bae23d34e9c75980b62c8ac6d5a5bb
José Ángel Martínez Torres <joseangelmt@outlook.com>
2020-01-02 13:22:27 +0100

    Added attribute to mark the assembly as a Reference Assembly

--- Digi21.OpenGis/Properties/AssemblyInfo.cs
+++ Digi21.OpenGis/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: ReferenceAssembly]
 [assembly: AssemblyTitle("Digi21.OpenGis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]

6d5612473c02860632905dfb915aebf45cd56883
José Ángel <joseangelmt@outlook.com>
2022-04-12 13:19:29 +0200

    Updated NuGet package

--- NuGet/CreateNugetPackages.cmd
+++ NuGet/CreateNugetPackages.cmd
@@ -1,3 +1,4 @@
 @echo off
 nuget.exe pack Digi21.OpenGis.nuspec
-nuget.exe sign Digi21.OpenGis.17.2.0.nupkg -CertificatePath dwo.pfx -TimeStamper http://tsa.starfieldtech.com
+nuget.exe sign Digi21.OpenGis.22.0.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://timestamp.comodoca.com?td=sha256.
+

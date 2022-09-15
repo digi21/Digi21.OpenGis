6bb9ea3291b78dc5c90a726a875e5b4126c8db3e
José Ángel <joseangelmt@outlook.com>
2022-09-15 18:13:09 +0200

    Versión 22.0.0.1 para compatibilidad con Digi3D compilado con Visual Studio 2022

--- NuGet/CreateNugetPackages.cmd
+++ NuGet/CreateNugetPackages.cmd
@@ -1,4 +1,4 @@
 @echo off
 nuget.exe pack Digi21.OpenGis.nuspec
-nuget.exe sign Digi21.OpenGis.22.0.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://timestamp.comodoca.com?td=sha256.
+nuget.exe sign Digi21.OpenGis.22.0.0.1.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://timestamp.comodoca.com?td=sha256.
 
